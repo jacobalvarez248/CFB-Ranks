777b84bf94ae70089d8461b8e5b1881324130bde
jacobalvarez248 <jacobalvarez248@gmail.com>
2025-07-21 13:22:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1843,7 +1843,13 @@ elif tab == "Team Dashboards":
         )
     
         st.altair_chart(chart, use_container_width=True)
- 
+     chart = alt.Chart(scatter_df).mark_circle(size=100, color='red').encode(
+            x='Off. Power Rating:Q',
+            y='Def. Power Rating:Q',
+            tooltip=['Team']
+        )
+        st.altair_chart(chart, use_container_width=True)
+
 elif tab == "Charts & Graphs":
     st.header("📈 Charts & Graphs")
     import altair as alt
