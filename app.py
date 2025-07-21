a0d3fc47c4207035a4782de06999dc2cb311cf8b
jacobalvarez248 <jacobalvarez248@gmail.com>
2025-07-21 15:49:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1836,6 +1836,7 @@ elif tab == "Team Dashboards":
     if not is_mobile():
         left_col, right_col = st.columns([1, 1])
         with left_col:
+            st.markdown("#### Conference Standings")
             st.markdown("".join(standings_html), unsafe_allow_html=True)
         with right_col:
             st.markdown("#### Offensive vs Defensive Power Rating")
