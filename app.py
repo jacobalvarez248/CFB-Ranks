8376785969b0991c913827960a04ea64df4b9a90
jacobalvarez248 <jacobalvarez248@gmail.com>
2025-07-21 08:25:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1055,15 +1055,17 @@ elif tab == "Team Dashboards":
                 <img src="{logo_url}" width="{logo_dim}" style="display:inline-block;vertical-align:middle;"/>
                 {f"<img src='{conf_logo_url}' width='{logo_dim}' style='display:inline-block; margin-left:0.5vw;vertical-align:middle;'/>" if conf_logo_url else ""}
             </div>
-            <div style="{dark_card}"><span style="font-size:0.8em;">Rank</span>{overall_rank}</div>
-            <div style="{dark_card}"><span style="font-size:0.8em;">Conf. Rk</span>{this_conf_rank}</div>
+            <div style="{card_base}">
+                <span style="font-size:0.8em;">Pwr. Rtg.</span>
+                <span style="line-height:1.15; font-weight:bold;">{power_rating_str}</span>
+            </div>
             <div style="{lighter_card}"><span style="font-size:0.8em;">6+</span>{at_least_6_pct_str}</div>
             <div style="{lighter_card}"><span style="font-size:0.8em;">8+</span>{at_least_8_pct_str}</div>
             <div style="{lighter_card}"><span style="font-size:0.8em;">10+</span>{at_least_10_pct_str}</div>
             <div style="{lighter_card}"><span style="font-size:0.8em;">12-0</span>{exact_12_pct_str}</div>
-            <div style="{card_base}"><span style="font-size:0.8em;">Ret.</span>{ret_prod_str}</div>
-            <div style="{card_base}"><span style="font-size:0.8em;">Off.</span>{off_ret_str}</div>
-            <div style="{card_base}"><span style="font-size:0.8em;">Def.</span>{def_ret_str}</div>
+            <div style="{card_base}"><span style="font-size:0.8em;">Ret. Prod.</span>{ret_prod_str}</div>
+            <div style="{card_base}"><span style="font-size:0.8em;">Off. Ret.</span>{off_ret_str}</div>
+            <div style="{card_base}"><span style="font-size:0.8em;">Def. Ret.</span>{def_ret_str}</div>
         </div>
         '''
     else:
